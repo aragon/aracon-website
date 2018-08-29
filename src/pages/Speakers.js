b028bc4708cd88a71ae7f40582bd9324bae9ab22
Delfina <delfinapolito@gmail.com>
2018-08-29 16:43:18 +0200

    eagle background

--- src/pages/Speakers.js
+++ src/pages/Speakers.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Page } from '../components'
+import { PageTitle, Page } from '../components'
 import Forms from '../components/Forms/Forms'
+import Background from '../components/Background/Background'
+
 import { Text } from '@aragon/ui'
 
 const Speakers = () => (
   <Page path="/speakers">
-      <Text size="xlarge">
-        <p>Volunteering at AraCon is a surefire way of making new connections and learning new skills, as well as experiencing the magic of Aragon’s flagship conference from an insider’s perspective.</p>
-      </Text>
-      <Text>
-        <p>AraCon is looking for work exchange volunteers who are interested in getting some behind the scenes experience putting on one of the most iconic blockchain events of the year. You could be assigned to do a number of various tasks that will ensure our guests and presenters have an exceptional experience.</p>
-      </Text>
+    <Background/>
+    <Text size="xlarge">
+      <p>Volunteering at AraCon is a surefire way of making new connections and learning new skills, as well as experiencing the magic of Aragon’s flagship conference from an insider’s perspective.</p>
+    </Text>
+    <Text>
+      <p>AraCon is looking for work exchange volunteers who are interested in getting some behind the scenes experience putting on one of the most iconic blockchain events of the year. You could be assigned to do a number of various tasks that will ensure our guests and presenters have an exceptional experience.</p>
+    </Text>
     <Forms type="speakers"/>
   </Page>
 )
