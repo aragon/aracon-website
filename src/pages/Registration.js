52cd3ac5e729e12aa141bf4b88b0efc8175c7f53
Delfina <delfinapolito@gmail.com>
2018-08-31 17:04:25 +0200

    Registration list

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -2,7 +2,23 @@ import React from 'react'
 import { Page } from '../components'
 import Section from '../components/Section/Section'
 import Background from '../components/Background/Background'
-import { Text, SafeLink } from '@aragon/ui'
+import { Text, SafeLink, theme } from '@aragon/ui'
+import styled from 'styled-components'
+
+const Criteria = styled.ul`
+  max-width: 632px;
+  text-align: left;
+  margin: auto;
+  padding-left: 50px;
+  margin-bottom: 23px;
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #707070;
+`
+
+
+
 
 const Registration = () => (
   <Page path="/registration">
@@ -14,15 +30,15 @@ const Registration = () => (
       </Text>
       <Text>
         <p>All other members must pay the standard admission fee.</p>
-        <p>
-          <span className="strong">Not sure whether you’re qualified for a free ticket? These are the criteria:</span><br/>
-          <span>You’re using Aragon to build your own DAO</span><br/>
-          <span>You have contributed to the development of the Aragon project and the Aragon Network</span><br/>
-          <span>You’re actively participating in discussions on our official community chat (aragon.chat)</span><br/>
-          <span>You have contributed in the creation of at least one Aragon Monthly issue</span><br/>
-          <span>You attended some of our previous events (hosted or supported)</span><br/>
-        </p>
+        <p><span className="strong">Not sure whether you’re qualified for a free ticket? These are the criteria:</span></p>
       </Text>
+      <Criteria>
+        <li>You’re using Aragon to build your own DAO</li>
+        <li>You have contributed to the development of the Aragon project and the Aragon Network</li>
+        <li>You’re actively participating in discussions on our official community chat (aragon.chat)</li>
+        <li>You have contributed in the creation of at least one Aragon Monthly issue</li>
+        <li>You attended some of our previous events (hosted or supported)</li>
+      </Criteria>
       <Text>
         <p>If you meet one of the criteria above, please email us with the proof at <SafeLink href="mailto:info@aracon.org"> info@aracon.org</SafeLink> and we will send you a registration link.<br/>If you are not a member of the Aragon community yet, we are excited to meet you soon. To get your ticket, please register below!</p>
       </Text>
