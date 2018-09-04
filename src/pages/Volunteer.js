c6992bcf0bb2778bf12a3f5bd40f51d105357924
Delfina <delfinapolito@gmail.com>
2018-09-04 13:51:46 +0200

    registration restyling

--- src/pages/Volunteer.js
+++ src/pages/Volunteer.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { Page } from '../components'
+import Section from '../components/Section/LandingSection'
+import { Text, SafeLink, theme } from '@aragon/ui'
+import styled from 'styled-components'
 import Forms from '../components/Forms/Forms'
-import Section from '../components/Section/Section'
-import Background from '../components/Background/Background'
-import { Text } from '@aragon/ui'
+
+const Container = styled.ul`
+  width: 100%;
+  padding: 50px 8.5%;
+`
 
 const Volunteer = () => (
   <Page path="/volunteer">
-    <Background/>
-    <Section>
-      <Text><h1>Volunteers</h1></Text>
-      <Text size="xlarge">
-        <p className="strong">Volunteering at AraCon is a surefire way of making new connections and learning new skills, as well as experiencing the magic of Aragon’s flagship conference from an insider’s perspective.</p>
-      </Text>
-      <Text>
-        <p>In exchange for your help, we’ll provide you with a free ticket to attend the event.</p>
-      </Text>
-      <Forms type="speakers" src="http://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2Bnex51b1b2jCZWBQtfTcaMTwCxvLp_2BTsBh0bYRiVb2M.js"/>
-    </Section>
+    <Container>
+      <Section>
+        <Text><div className="h1box"><h1>Volunteer</h1></div></Text>
+        <Text size="xlarge">
+          <h3>Volunteering at AraCon is a surefire way of making new connections and learning new skills, as well as experiencing the magic of Aragon’s flagship conference from an insider’s perspective.</h3>
+        </Text>
+        <Text>
+          <p>In exchange for your help, we’ll provide you with a free ticket to attend the event.</p>
+        </Text>
+        <br/>
+        <Forms type="speakers" src="http://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2Bnex51b1b2jCZWBQtfTcaMTwCxvLp_2BTsBh0bYRiVb2M.js"/>
+      </Section>
+    </Container>
   </Page>
 )
 
