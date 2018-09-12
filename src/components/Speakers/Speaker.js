6b27cc598cd42983caac0b663c6d215ef406e927
Delfina <delfinapolito@gmail.com>
2018-09-12 18:53:18 +0200

    Making speakers section responsive

--- src/components/Speakers/Speaker.js
+++ src/components/Speakers/Speaker.js
@@ -66,7 +66,7 @@ const SpeakerBox = styled.div`
   width: 100%;
   min-height: 244px;
   max-width: 350px;
-  ${medium('max-width: auto;')};
+  ${medium('max-width: 100%;')};
   height: auto;
   .speaker-card {
     text-align: left;
