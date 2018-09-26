b57872641d5d0178e5bbd7adc42e0727c11adb95
Smokyish <smoky@kaerki.eu>
2018-09-26 19:48:29 +0300

    add date more prominently

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -49,6 +49,10 @@ const HeroBox = styled.div`
     ${medium('transform: rotate(-90deg);')};
     transform: rotate(0deg);
   }
+
+  p {
+    color: rgba(255,255,255,1):
+  }
 `;
 
 const HeroTriangle = styled.div`
@@ -74,6 +78,7 @@ const Hero = () => (
     <HeroBox>
       <Logo src={araconLogo} alt="AraCon" />
       <h2>BUILDING ORGANIZATIONS & GOVERNANCE OF THE FUTURE</h2>
+      <p>January 29th-30th 2019, Berlin</p>
       <h2 className="sides right">Berlin, Germany</h2>
       <h2 className="sides left">Jan 29-30.2019</h2>
       <HeroTriangle/>
