1f0f21e407d1150fd7423375d52bed251f1caea6
Delfina <delfinapolito@gmail.com>
2018-09-26 17:02:16 +0100

    Using CSS filters to modify speakers photos on hover

--- src/components/Home/Speaker.js
+++ src/components/Home/Speaker.js
@@ -4,6 +4,7 @@ import { Card, Text, theme, breakpoint } from '@aragon/ui'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 const medium = css => breakpoint('medium', css);
+import includee from '../Speakers/assets/GriffGreen.png';
 
 const TextBox = styled.div`
   margin-top: 12px;
@@ -24,21 +25,45 @@ const TextBox = styled.div`
   }
 `;
 
-const Photo = styled.img`
+const Photo = styled.div`
   width: 100%;
   overflow: hidden;
-  -webkit-transition: filter 1s ease-in-out;
-  -moz-transition: filter 1s ease-in-out;
-  -o-transition: filter 1s ease-in-out;
-  transition: filter 1s ease-in-out;
-  object-fit: cover;
   height: 238px;
-  filter: grayscale(100%);
-  -webkit-filter: grayscale(100%);
+  background-image: linear-gradient(0deg, #233145, #233145),  url(${props => props.src});
+  background-blend-mode: color;
+  background-position: center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
 
-  &:hover {
-    filter: grayscale(0%);
-    -webkit-filter: grayscale(0%);
+  &:before {
+    width: 100%;
+    overflow: hidden;
+    height: 238px;
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-image: url(${props => props.src});
+    background-position: center;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    border-radius: inherit; 
+    opacity: 0;
+    -webkit-transition: all 0.4s ease-in-out;
+    -moz-transition: all 0.4s ease-in-out;
+    -o-transition: all 0.4s ease-in-out;
+    transition: all 0.4s ease-in-out;
+  }
+
+  &:hover:before {
+   opacity: 1
   }
 `;
 
@@ -62,7 +87,7 @@ class Speaker extends React.Component {
       <Card onMouseEnter={() => this.setState({ picture: photo + '.png' })}
           onMouseLeave={() => this.setState({ picture: photo + '_.png' })}>
         <Link to={'/speakers#' + linkTo}>
-          <Photo src={require(`../Speakers/assets/${this.props.photo + '.png'}`)}/>
+          <Photo src={require('../Speakers/assets/' + photo + '.png')}/>
         </Link>
         <TextBox>
           <Text><p>{name}</p></Text>
--- src/pages/Speakers.js
+++ src/pages/Speakers.js
@@ -77,29 +77,29 @@ const Speakers = () => (
       <SpeakersBox>
         <div className="h1box"><h1>Speakers</h1></div>
         <Container>
-          <Speaker photo="Luis Cuende" linkTo="luis" name="Luis Cuende" position="CEO" company="Aragon One"
+          <Speaker photo="LuisCuende" linkTo="luis" name="Luis Cuende" position="CEO" company="Aragon One"
             bio="Luis is the CEO of Aragon One, one of the teams working on the Aragon project. Luis has been
             into crypto since 2011, and loves how decentralized organizations can solve the world’s worst
             problems. Luis has been awarded as the best underage European programmer in 2011, is a Forbes 30
             Under 30, a MIT TR35 and was an Advisor to the VP of the European Commission. He cofounded the
             blockchain startup Stampery. Prior to founding startups, he created the world's first Linux
             distribution with facelogin."/>
-          <Speaker photo="Griff Green" linkTo="griff" name="Griff Green" position="Co-founder" company="Giveth & Aragon DAC"
+          <Speaker photo="GriffGreen" linkTo="griff" name="Griff Green" position="Co-founder" company="Giveth & Aragon DAC"
             bio="Community manager for TheDAO, led the cleanup effort for TheDAO Hack from every angle, co-founder of
             the White Hat Group and Giveth and leading the creation of the Aragon DAC, as well as advising DAppNode,
             0KIMS (iden3), ETHSignals, Open Source Blockchain Explorers NOW! and many other groups within the Ethereum
             Community."/>
-          <Speaker photo="Jutta Steiner" linkTo="jutta" name="Jutta Steiner" position="Co-Founder and CEO" company="Parity Technologies"
+          <Speaker photo="JuttaSteiner" linkTo="jutta" name="Jutta Steiner" position="Co-Founder and CEO" company="Parity Technologies"
             bio="Dr Jutta Steiner is the co-founder and CEO of Parity Technologies, a blockchain technology company best
             known for Parity Ethereum, the most advanced Ethereum client. Previously, Jutta served as Chief of Security
             for the Ethereum Foundation and pioneered blockchain use in supply chains as Project Provenance co-founder."/>
-          <Speaker photo="Trent McConaghy" linkTo="trent" name="Trent-McConaghy" position="Co-founder" company="BigchainDB"
+          <Speaker photo="TrentMcConaghy" linkTo="trent" name="Trent-McConaghy" position="Co-founder" company="BigchainDB"
             bio="Trent McConaghy did AI research for two decades, including machine creativity and
             AI to help drive Moore's Law, via two startups (both acquired) and a PhD. Now he
             focuses on AI * blockchain, via Ocean to democratize AI data, and BigchainDB
             decentralized database. His hobbies include Token Engineering and advising governments
             on AI * blockchain"/>
-          <Speaker photo="Santiago Siri" linkTo="santiago" name="Santiago Siri" position="Founder" company="Democracy Earth Foundation"
+          <Speaker photo="SantiagoSiri" linkTo="santiago" name="Santiago Siri" position="Founder" company="Democracy Earth Foundation"
             bio="Founder of Democracy Earth Foundation, a Y Combinator backed non-profit building
             incorruptible digital governance technology; and the Partido de la Red (Peers Party),
             the first digital political party that ran for elections with candidates committed to
@@ -107,38 +107,38 @@ const Speakers = () => (
             Published 'Hacktivismo' in 2015 with Random House. Member of the World Economic Forum
             since 2012. Awarded by MIT Technology Review and Singularity University. Co-founded
             Argentine Game Developers Association in 2002."/>
-          <Speaker photo="Joe Urgo" linkTo="joe" name="Joe Urgo" position="Co-Founder" company="District0x"
+          <Speaker photo="JoeUrgo" linkTo="joe" name="Joe Urgo" position="Co-Founder" company="District0x"
             bio="Joe is a co-founder of district0x, a network of decentralized marketplaces and CEO at Sourcerers,
             a consultancy firm supporting leading Ethereum-based projects. Joe previously spent three years
             as an Operations Manager at Coinbase. Prior to Coinbase, he was a derivatives trader for Three
             Arrows Capital, an international hedge fund based in Singapore. Joe serves as an advisor at Status,
             Keep, SpankChain, Bloom, Witnet, Abacus, Access Network, LiquidX, Fabric Ventures, Republic, and
             Common Interest."/>
-          <Speaker photo="Lane Rettig" linkTo="lane" name="Lane Rettig" position="Independent Core Developer" company="Ewasm"
+          <Speaker photo="LaneRettig" linkTo="lane" name="Lane Rettig" position="Independent Core Developer" company="Ewasm"
             bio="Lane Rettig is an independent Ethereum core developer and a member of the Ewasm team, where he
             is helping build the execution engine for Ethereum 2.0 (the Shasper roadmap). He is also the founder
             of Crypto NYC, a Manhattan-based, blockchain-focused community and coworking space dedicated to making
             Web3 technology accessible to all humans."/>
-          <Speaker photo="Primavera De Filippi" linkTo="primavera" name="Primavera De Filippi" position="Postdoctoral researcher" company="National Center of Scientific Research (CNRS)"
+          <Speaker photo="PrimaveraDeFilippi" linkTo="primavera" name="Primavera De Filippi" position="Postdoctoral researcher" company="National Center of Scientific Research (CNRS)"
             bio="Primavera De Filippi is a permanent researcher at the National Center of Scientific
             Research (CNRS) in Paris, a faculty associate at the Berkman-Klein Center for Internet &
             Society at Harvard University, and a Visiting Fellow at the Robert Schuman Centre for
             Advanced Studies at the European University Institute."/>
-          <Speaker photo="Mike Goldin" linkTo="mike" name="Mike Goldin" position="Lead Engineer" company="Cryptosystems Productization Lab, ConsenSys"
+          <Speaker photo="MikeGoldin" linkTo="mike" name="Mike Goldin" position="Lead Engineer" company="Cryptosystems Productization Lab, ConsenSys"
             bio="Mike is the author of “token-curated registries 1.0”, a widely-cited paper which
             originally formalized the basic design and properties of such cryptosystems. He launched
             adChain and implemented its token sale, which sold out in one block. He has overseen and
             facilitated the development of a widely-used TCR implementation, as well as multiple
             graphical user interfaces for TCRs. He graduated from Columbia University with a degree
             in computer science."/>
-          <Speaker photo="Matt Lockyer" linkTo="matt" name="Matt Lockyer" position="Founder" company="Soloblock Solutions"
+          <Speaker photo="MattLockyer" linkTo="matt" name="Matt Lockyer" position="Founder" company="Soloblock Solutions"
             bio="Matt Lockyer wrote the Ethereum ERC-998 standard for Composable Tokens and his work
             on Token Curated Registries (TCRs) has been published on a16z's crypto reading list. Matt
             founded and operates the NFTy Magicians - a group of over 300 blockchain developers - as a
             pendant to the Ethereum Foundation. He currently advises projects on their smart contract
             architecture and token engineering processes with a background in full stack development,
             computer science, UX research, public speaking and university lecturing."/>
-          <Speaker photo="Mario Laul" linkTo="mario" name="Mario Laul" position="Researcher" company="Placeholder Ventures"
+          <Speaker photo="MarioLaul" linkTo="mario" name="Mario Laul" position="Researcher" company="Placeholder Ventures"
             bio="Mario Laul works as a researcher for Placeholder, a New York venture capital firm investing
             in decentralized information networks, and for Beyond the Technological Revolution, a four-year
             research project led by Carlota Perez exploring the relationship between technology, finance,
@@ -146,23 +146,23 @@ const Speakers = () => (
             the Estonian Academy of Arts in Tallinn, and is looking to combine the study of technology governance
             with a sociologically informed perspective on emerging forms of decentralized economic
             and social coordination."/>
-          <Speaker photo="Martin Koppelmann" linkTo="martin" name="Martin Köppelmann" position="CEO and Co-founder" company="Gnosis"
+          <Speaker photo="MartinKoppelmann" linkTo="martin" name="Martin Köppelmann" position="CEO and Co-founder" company="Gnosis"
             bio="Martin Köppelmann has been an entrepreneur in the blockchain space for more than 5 years.
             He is the CEO and co-founder of Gnosis, the decentralized Gnosis prediction market - one of the
             first decentralized application that went live on Ethereum in 2015 in an early verison."/>
-          <Speaker photo="Raine Revere" linkTo="raine" name="Raine Revere" position="Co-foundeer" company="Maiden"
+          <Speaker photo="RaineRevere" linkTo="raine" name="Raine Revere" position="Co-foundeer" company="Maiden"
             bio="Raine Revere is an engineer and educator in the Ethereum community who has spoken
             at blockchain events around the world. Raine is the Lead Architect of ShapeShift Prism
             and is the co-founder of Maiden, a company that delivers socially-aware blockchain
             education, cultural & leadership development, and diversity-powered blockchain
             consulting. As a therapist, teacher, and cryptocurrency engineer, Raine has the unique
             capacity to engage deeply with both the technical and social aspects of technology."/>
-          <Speaker photo="Aron Fischer" linkTo="aron" name="Aron Fischer" position="Governance Researcher" company="Colony"
+          <Speaker photo="AronFischer" linkTo="aron" name="Aron Fischer" position="Governance Researcher" company="Colony"
             bio="Aron Fischer received his PhD in mathematics from the City University of New York in 2015. He is working
             in the research arm of Colony: a platform for open organizations which seeks to enable new forms of
             decentralised organizations. The work involves a careful balancing of what is desirable
             in terms of governance and what is possible given the technical limitations of the Ethereum blockchain."/>
-          <Speaker photo="Maria Gomez" linkTo="maria" name="María Gómez" position="Ecosystem Development" company="Aragon One"
+          <Speaker photo="MariaGomez" linkTo="maria" name="María Gómez" position="Ecosystem Development" company="Aragon One"
             bio="María Gómez is a former corporate lawyer. She worked several years in the M&A and corporate finance
             practice. Currently she works on ecosystem development for Aragon.one, one of the teams working for the
             Aragon project. "/>
