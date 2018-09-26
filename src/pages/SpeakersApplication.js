844eb0b3b6b7879fb528911278e26ee25b05935e
Delfina <delfinapolito@gmail.com>
2018-09-26 18:50:58 +0100

    Fix on forms overflow

--- src/pages/SpeakersApplication.js
+++ src/pages/SpeakersApplication.js
@@ -9,19 +9,24 @@ const large = css => breakpoint('large', css);
 
 const TextContainer = styled.div`
   width: 100%;
-  margin: auto;
   max-width: 695px;
-  padding: 50px 0 50px 8.5%;
+  padding: 50px 0 50px 0;
   text-align: left;
-  ${large('position: absolute; width: 41%; margin: inherit')};
+  ${large('width: 41%;')};
 `;
 
 const OuterContainer = styled.div`
-  width: 100%;
   position: relative;
   max-width: 1540px;
   margin: auto;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  ${large('justify-content: space-between; max-width: 1440px !important; margin: auto; flex-direction: row;')};
+  padding-top: 40px;
+  width: 83%;
 `;
+
 
 const SpeakersApplication = () => (
   <Page path="/speakersapplication">
@@ -31,9 +36,9 @@ const SpeakersApplication = () => (
           <div className="h1box" id="speakers-form"><h1>Speakers application</h1></div>
           <p>If you would like to join a panel or do a talk at AraCon on anything related to governance, blockchain, future of work or borderless collaboration - please apply here before November 10th.</p>
         </TextContainer>
+        <Forms type="speakers" src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2Bnex51b1b2jCZWBQtfTcaOhfvbt_2Fq8nWc81qWg3efVA.js"/>
+        <div id="speakers"/>
       </OuterContainer>
-      <Forms type="speakers" src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2Bnex51b1b2jCZWBQtfTcaOhfvbt_2Fq8nWc81qWg3efVA.js"/>
-      <div id="speakers"/>
     </Section>
   </Page>
 );
