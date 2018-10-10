aa5985557aca0b808f05f7bb912862bbbaf3e4f8
Smokyish <smoky@kaerki.eu>
2018-10-10 13:40:11 +0300

    change to dark theme

--- src/pages/Scholarship.js
+++ src/pages/Scholarship.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import {Page} from '../components'
+import { Page } from '../components'
 import Section from '../components/Section/Section'
-import {Text, SafeLink, theme, breakpoint} from '@aragon/ui'
+import { Text, SafeLink, theme, breakpoint } from '@aragon/ui'
 import styled from 'styled-components'
 import Forms from '../components/Forms/Forms'
 
 const large = css => breakpoint('large', css);
 
-const OuterContainer = styled.div `
+const TextContainer = styled.div`
+  width: 100%;
+  max-width: 695px;
+  padding: 50px 0 50px 0;
+  text-align: left;
+  ${large('width: 41%;')};
+  p {
+    color: #dad7d7;
+  }
+  h3 {
+    color: #dad7d7;
+  }
+`;
+
+const OuterContainer = styled.div`
   position: relative;
   max-width: 1540px;
   margin: auto;
@@ -19,38 +33,26 @@ const OuterContainer = styled.div `
   width: 83%;
 `;
 
-const Container = styled.div `
-  width: 100%;
-  margin: 0;
-  max-width: 695px;
-  padding: 50px 0 50px 0;
-  text-align: left;
-  ${large('width: 41%; margin: inherit')};
-`;
 
 const Scholarship = () => (<Page path="/scholarships">
-  <Section className="light light-form all-page">
-    <OuterContainer>
-      <Container>
-        <Text>
-          <div className="h1box">
-            <h1>Scholarships</h1>
-          </div>
-        </Text>
+<Section className="dark dark-form all-page">
+  <OuterContainer>
+    <TextContainer>
+      <div className="h1box" id="speakers-form"><h1>Scholarships</h1></div>
         <Text size="xlarge">
-          <h3>Scholarships</h3>
+          <h3>Need financial help attending AraCon?</h3>
         </Text>
         <Text>
-          <p><br/>Need financial help attending AraCon?<br/>
-            We will be announcing a scholarship program in October to provide financial assistance for those in need.<br/>
-            <SafeLink href="https://eepurl.com/dHogTD" target="_blank">Subscribe to the AraCon email newsletter</SafeLink>
-            to stay tuned for details</p>
-        </Text>
-        <br/>
-      </Container>
-      <Forms type="speakers" src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2Bnex51b1b2jCZWBQtfTcaMTwCxvLp_2BTsBh0bYRiVb2M.js"/>
-    </OuterContainer>
-  </Section>
-</Page>);
+          <p>We will be announcing a scholarship program in October to provide financial assistance for those in need.<br/>
+          <SafeLink href="https://eepurl.com/dHogTD" target="_blank">Subscribe to the AraCon email newsletter</SafeLink> to stay tuned for details</p><br />
+          <p><h1>Scholarship Partners</h1></p>
+          <p><img src="https://district0x.io/images/logo@2x.png" alt="district0x" /></p>
+          </Text>
+          <br/>
+          </TextContainer>
+        <Forms type="speakers" src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2Bnex51b1b2jCZWBQtfTcaOhfvbt_2Fq8nWc81qWg3efVA.js"/>
+      </OuterContainer>
+    </Section>
+  </Page>);
 
 export default Scholarship
