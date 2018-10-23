b339c91a083f8e6513f6ee1cc566bb906aba878c
Delfina <delfinapolito@gmail.com>
2018-10-23 15:06:05 +0200

    New design for scholarship page

--- src/pages/Scholarship.js
+++ src/pages/Scholarship.js
@@ -4,6 +4,10 @@ import Section from '../components/Section/Section'
 import { Text, SafeLink, theme, breakpoint } from '@aragon/ui'
 import styled from 'styled-components'
 import Forms from '../components/Forms/Forms'
+import scholarship1 from './assets/scholarship1.svg'
+import scholarship2 from './assets/scholarship2.svg'
+import scholarship3 from './assets/scholarship3.svg'
+import agenda from './assets/agenda.svg'
 
 const large = css => breakpoint('large', css);
 
@@ -35,6 +39,42 @@ const OuterContainer = styled.div`
   ${large('justify-content: space-between; max-width: 1440px !important; margin: auto; flex-direction: row;')};
   padding-top: 40px;
   width: 83%;
+  p.blue {
+    color: #a1a5ca;
+  }
+  blockquote {
+    position: relative;
+    position: relative;
+    border-left: solid 5px #18ebd6;
+    margin-left: 0;
+    padding-left: 40px;
+  }
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: space-between;
+  margin: 30px -15px;
+  p {
+    padding-top: 10px;
+  }
+  div {
+    padding: 15px;
+    width: 100%;
+  }
+  &.agenda-container {
+    margin-top: 10px;
+    .agenda-box {
+      display: flex;
+      p {
+        padding-top: 0;
+      }
+      img {
+        margin-right: 15px
+      }
+    }
+  }
 `;
 
 
@@ -43,26 +83,38 @@ const Scholarship = () => (<Page path="/scholarships">
   <OuterContainer>
     <TextContainer>
       <div className="h1box" id="speakers-form"><h1>Scholarships</h1></div>
-        <Text size="xlarge">
-          <h3>Excited to come to AraCon, but don't have the financial resources?</h3>
-          <p><b>Fill out this form and let us help.</b></p><br />
-        </Text>
+        <blockquote><h3>Excited to come to AraCon, but don&#39;t have the financial resources?</h3></blockquote>
         <Text>
-          <p>Our vision for this conference is to gather Aragon’s global community together. Therefore we want everyone who's super interested in joining the event to be able to attend. The scholarship recipients will get a conference ticket, round trip flights and accommodation for the duration of the conference.</p><br />
-          <p>
-          Everyone is welcome to apply. We will prioritize scholarships to:<br />
-        <b>1.</b> Applicants who have contributed the most to the Aragon community and are in financial hardship<br />
-        <b>2.</b> Applicants from emerging countries/regions where the average wage is lower<br />
-        <b>3.</b> Applicants who'll be using Aragon's platform in the future.</p><br />
-        <p><b>Deadline to apply:</b> November 23rd 2018. We will be reviewing the applications right after and will contact you accordingly.</p>
-          <p><h1>Scholarship Partners</h1></p>
-          <p><img src="https://district0x.io/images/logo@2x.png" alt="district0x" /></p>
-          </Text>
-          <br/>
-          </TextContainer>
-        <Forms type="speakers" src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd30OdvFNIdQfI6k_2FSnIMsLHwkqqfzqgEK49Yj3zmHyG4.js"/>
-      </OuterContainer>
-    </Section>
-  </Page>);
+          <p className="blue">Our vision for this conference is to gather Aragon’s global community together. Therefore we want everyone who&#39;s super interested in joining the event to be able to attend. The scholarship recipients will get a conference ticket, round trip flights and accommodation for the duration of the conference.</p><br />
+          <p className="blue">Everyone is welcome to apply. We will prioritize scholarships to:</p>
+        </Text>
+        <FlexContainer>
+          <div>
+            <img src={scholarship1} />
+            <p>Applicants who have contributed the most to the Aragon community and are in financial hardship.</p>
+          </div>
+          <div>
+            <img src={scholarship2} />
+            <p>Applicants from emerging countries / regions where the average wage is lower.</p>
+          </div>
+          <div>
+            <img src={scholarship3} />
+            <p>Applicants who'll be using Aragon's platform in the future.</p>
+          </div>
+        </FlexContainer>
+        <Text>
+          <p className="blue">Deadline to apply:</p>
+        </Text>
+        <FlexContainer className="agenda-container">
+          <div className="agenda-box">
+            <img src={agenda} />
+            <p>November 23rd 2018. We will be reviewing the applications right after and will contact you accordingly.</p>
+          </div>
+        </FlexContainer>
+      </TextContainer>
+      <Forms type="speakers" src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd30OdvFNIdQfI6k_2FSnIMsLHwkqqfzqgEK49Yj3zmHyG4.js"/>
+    </OuterContainer>
+  </Section>
+</Page>);
 
 export default Scholarship
