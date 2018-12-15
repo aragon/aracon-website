82fe9b319ebbe12e0cf4045f65935be3aa1ed997
delfipolito <delfinapolito@gmail.com>
2018-12-14 22:08:19 -0300

    Updates

--- react-static-routes.js
+++ react-static-routes.js
@@ -23,15 +23,17 @@ const universalOptions = {
 const t_1 = universal(import('../src/pages/Speakers'), universalOptions)
 const t_2 = universal(import('../src/pages/SpeakerApplication'), universalOptions)
 const t_3 = universal(import('../src/pages/EmptyAgenda'), universalOptions)
-const t_4 = universal(import('../src/pages/Registration'), universalOptions)
-const t_5 = universal(import('../src/pages/Volunteer'), universalOptions)
-const t_6 = universal(import('../src/pages/Contact'), universalOptions)
-const t_7 = universal(import('../src/pages/ContactThanks'), universalOptions)
-const t_8 = universal(import('../src/pages/Faq'), universalOptions)
-const t_9 = universal(import('../src/pages/Travel'), universalOptions)
-const t_10 = universal(import('../src/pages/Scholarship'), universalOptions)
-const t_11 = universal(import('../src/pages/Livestream'), universalOptions)
-const t_12 = universal(import('../src/pages/NotFound'), universalOptions)
+const t_4 = universal(import('../src/pages/Agenda'), universalOptions)
+const t_5 = universal(import('../src/pages/Registration'), universalOptions)
+const t_6 = universal(import('../src/pages/Volunteer'), universalOptions)
+const t_7 = universal(import('../src/pages/Contact'), universalOptions)
+const t_8 = universal(import('../src/pages/ContactThanks'), universalOptions)
+const t_9 = universal(import('../src/pages/Faq'), universalOptions)
+const t_10 = universal(import('../src/pages/Travel'), universalOptions)
+const t_11 = universal(import('../src/pages/SatelliteEvents'), universalOptions)
+const t_12 = universal(import('../src/pages/Scholarship'), universalOptions)
+const t_13 = universal(import('../src/pages/Livestream'), universalOptions)
+const t_14 = universal(import('../src/pages/NotFound'), universalOptions)
 
 
 // Template Map
@@ -48,12 +50,14 @@ t_8,
 t_9,
 t_10,
 t_11,
-t_12
+t_12,
+t_13,
+t_14
 ]
 
 // Template Tree
 global.templateIDsByPath = global.templateIDsByPath || {
-  '404': 12
+  '404': 14
 }
 
 // Get template for given path
