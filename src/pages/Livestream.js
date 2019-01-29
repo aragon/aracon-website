708c1b287d9206f8eda4c7e6c0493a45851c9e74
Pierre Bertet <hello@pierre.world>
2019-01-29 10:55:11 +0100

    youtube

--- src/pages/Livestream.js
+++ src/pages/Livestream.js
@@ -5,19 +5,19 @@ import { Text, Button, theme, breakpoint } from '@aragon/ui'
 import styled from 'styled-components'
 import live from '../components/Home/assets/live.svg'
 
-const medium = css => breakpoint('medium', css);
-const large = css => breakpoint('large', css);
+const medium = css => breakpoint('medium', css)
+const large = css => breakpoint('large', css)
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
   margin: 0;
-  background: #18181A;
+  background: #18181a;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  min-height: calc(100vh - 116px)!important;
+  min-height: calc(100vh - 116px) !important;
   h1 {
     text-align: center;
     margin-top: 30px;
@@ -32,19 +32,25 @@ const Container = styled.div`
     text-align: center;
     color: #dad7d7;
   }
-
-`;
+`
 
 class Livestream extends React.Component {
-  state = { active: 'dayOne' };
+  state = { active: 'dayOne' }
 
   render() {
-    const { active } = this.state;
+    const { active } = this.state
     return (
       <Page path="/livestream">
         <Section className="dark all-page">
           <Container>
-            <iframe src="https://d1sehmygbqvrdw.cloudfront.net/embed/0x92A4Fa40edd4931DB19Fbb48751B270b1dE55997" frameBorder="0" allow="autoplay; encrypted-media"  allowFullScreen></iframe>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube-nocookie.com/embed/k-ZoXV6D4H4"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
           </Container>
         </Section>
       </Page>
